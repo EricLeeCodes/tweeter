742a0351ce11e41efc0bdfe42ff189a6605e2e00
Eric Lee <ericleecodes@gmail.com>
2024-02-28 13:39:33 -0800

    Remove console.log from composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   $charCount.on('input', function() {
     let charLength = $(this).val().length;
     $charCounter.text(140 - charLength);
-    console.log($("#char-counter").text(140 - charLength));
     if (charLength >= 130) {
       $charCounter.css("color", "red");
     } else if (charLength <= 130) {
