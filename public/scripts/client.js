ab7c4d3a1aed29cc0864f298d244a4d8881b38b7
Eric Lee <ericleecodes@gmail.com>
2024-02-28 16:27:36 -0800

    Implement dynamic tweets using jQuery on client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,84 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(() => {
+  const $tweetsContainer = $('#tweets-container');
 
+  //Sample tweet database
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  //Rendering tweet and appending them to our front end.
+  const renderTweets = (tweetData) => {
+    $tweetsContainer.empty(); //Empty before adding anything to display
+
+    //Loop over the data and create tweet with the informations
+    for (const tweet of tweetData) {
+      const $tweet = createTweet(tweet); //Assign to jQ variable 
+      $tweetsContainer.append($tweet); //Append to display page
+    }
+
+  };
+
+  //Building out the tweet structure
+  const createTweet = (tweet) => {
+    const tweetUser = tweet.user;
+    const tweetContent = tweet.content;
+
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div>
+            <div>
+              <img src=${tweetUser.avatars}/>
+              <p>${tweetUser.name}</p>
+            </div>
+            <div>
+              <p>${tweetUser.handle}</p>
+            </div>
+          </div>
+          <div class="text-display">
+            <p>${tweetContent.text}</p>
+          </div>
+        </header>
+        <footer>
+          <div>
+            <p>${tweet["created_at"]} days ago</p>
+          </div>
+          <div class="icons">
+            <div class="icon"><i class="fa-solid fa-flag"></i></div>
+            <div class="icon"><i class="fa-solid fa-retweet"></i></div>
+            <div class="icon"><i class="fa-solid fa-heart"></i></div>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  };
+
+
+  renderTweets(tweetData);
+});
